ab1ad47799472b8b98246af2d835a1624ff8f6a7
Camilo Caceres <camiloac97@gmail.com>
2022-05-24 21:43:40 -0500

    ui: create BibleContainer

---
+++ components/BibleContainer.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const vulgata = {
+  name: 'Vulgata',
+  text: 'et ait : Faciamus hominem ad imaginem et similitudinem nostram : et praesit piscibus maris, et volatilibus caeli, et bestiis, universaeque terrae, omnique reptili, quod movetur in terra. Et creavit Deus hominem ad imaginem suam : ad imaginem Dei creavit illum, masculum et feminam creavit eos.',
+  quote: 'Gn 1, 26-27',
+}
+
+const septuaginta = {
+  name: 'Septuaginta',
+  text: 'et ait : Faciamus hominem ad imaginem et similitudinem nostram : et praesit piscibus maris, et volatilibus caeli, et bestiis, universaeque terrae, omnique reptili, quod movetur in terra. Et creavit Deus hominem ad imaginem suam : ad imaginem Dei creavit illum, masculum et feminam creavit eos.',
+  quote: 'Gn 1, 26-27',
+}
+const bibles = [vulgata, septuaginta, vulgata]
+
+</script>
+
+<template>
+  <div class="flex justify-center space-x-8">
+    <div v-for="bible in bibles" :key="bible.name">
+      <DisplayBible :name="bible.name" :text="bible.text" :quote="bible.quote" />
+    </div>
+  </div>
+  <button class="my-5 rounded-full p-2 bg-green-500 text-green-700 dark:text-green-100 dark:bg-green-600 shdow-lg hover:bg-green-400 hover:text-green-600 transition transform hover:rotate-180">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+    </svg>
+  </button>
+</template>
---
+++ components/DisplayBible.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: String,
+  text: String,
+  quote: String,
+})
+</script>
+
+<template>
+  <div class="h-96 w-80 shadow-2xl rounded-lg">
+    <h1 class="p-2 text-xl text-gray-800 font-bold">
+      {{ props.name }}
+    </h1>
+    <p class="p-2 mx-2 text-left text-gray-600">
+      {{ props.text }}
+    </p>
+    <p class="p-2 text-right mx-2 text-gray-500">
+      {{ props.quote }}
+    </p>
+  </div>
+</template>
